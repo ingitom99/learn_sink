239687e1411b90966b69aa122ad9249a7ff6a72f
ingitom99 <59366443+ingitom99@users.noreply.github.com>
2023-05-11 12:48:54 +0000

    adding cost matrix function file

---
+++ cost_matrices.py
@@ -0,0 +1,15 @@
+from torch import zeros
+
+def euclidean_cost_matrix(width, height, normed=True):
+  n = width*height
+  M = zeros([n, n])
+  for a in range(n):
+    for b in range(n):
+      ax = a // width
+      ay = a % width
+      bx = b // width
+      by = b % width
+      M[a][b] = (ax - bx)**2 + (ay - by)**2
+  if (normed == True):
+    M = M / M.max()
+  return M
