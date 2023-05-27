b5f051496fee3b224c2cd45981a27e7bd23a25f6
ingitom99 <59366443+ingitom99@users.noreply.github.com>
2023-05-27 13:38:03 +0000

    update euclidean cost matrix function

--- cost_matrices.py
+++ cost_matrices.py
@@ -1,15 +1,22 @@
+# Imports
 import torch
 
-def euclidean_cost_matrix(width, height, normed=True):
+def euclidean_cost_matrix(width : int, height : int , normed: bool=True) -> torch.Tensor:
+  """
+  Returns a cost matrix for the euclidean distance between pixels in an image of size width x height
+  """
   n = width*height
   M = torch.zeros([n, n])
+
   for a in range(n):
     for b in range(n):
       ax = a // width
       ay = a % width
       bx = b // width
       by = b % width
-      M[a][b] = (ax - bx)**2 + (ay - by)**2
+      M[a][b] = ((ax - bx)**2 + (ay - by)**2)*.5
+
   if (normed == True):
     M = M / M.max()
+    
   return M
