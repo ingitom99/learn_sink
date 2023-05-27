d332673bae28608ab4d417c3b44b991296774493
ingitom99 <59366443+ingitom99@users.noreply.github.com>
2023-05-27 13:53:03 +0000

    update nets

--- nets.py
+++ nets.py
@@ -1,26 +1,19 @@
+# Imports
 import torch
 import torch.nn as nn
-from torchvision.transforms import Resize
 
-class pred_net(nn.Module):
-
-  def __init__(self, dim):
-    super(pred_net, self).__init__()
-    self.dim = dim
-    self.l1 = nn.Sequential(nn.Linear(2*dim, 4*dim), nn.BatchNorm1d(4*dim), nn.ELU())
-    self.l2 = nn.Sequential(nn.Linear(4*dim, 4*dim), nn.BatchNorm1d(4*dim), nn.ELU())
-    self.l3 = nn.Sequential(nn.Linear(4*dim, 4*dim), nn.BatchNorm1d(4*dim), nn.ELU())
-    self.l4 = nn.Sequential(nn.Linear(4*dim, dim))
-    self.layers = [self.l1, self.l2, self.l3, self.l4]
-
-  def forward(self, x):
-    for layer in self.layers:
-      x = layer(x)
-    return x
-  
+# Generative network class
 class gen_net(nn.Module):
-  
-  def __init__(self, dim_prior, dim, dust_const, skip_const):
+  """
+  Generative network class to create a data-creating neural network for the training process.
+  """
+  def __init__(self, dim_prior : int, dim : int, dust_const : float, skip_const : float):
+    """
+    dim_prior: dimension of the prior sample (i.e. the latent space)
+    dim: dimension of the data
+    dust_const: constant to add to the output of the network to prevent the data from containing zeros
+    skip_const: constant to control influence of the skip connection
+    """
     super(gen_net, self).__init__()
     self.dim_prior = dim_prior
     self.dim = dim
@@ -32,14 +25,27 @@ class gen_net(nn.Module):
     self.layers = [self.l1]
 
   def forward(self, x):
+
+    # Creating a reshaped copy of the input to use as a skip connection
     x_0 = x.detach().clone().reshape(2, x.size(0), self.length_prior, self.length_prior)
-    transform = Resize((self.length, self.length), antialias=True)
+    transform = torchvision.transforms.Resize((self.length, self.length), antialias=True)
     x_0 = torch.cat((transform(x_0[0]).reshape(x.size(0), self.dim), transform(x_0[1]).reshape(x.size(0), self.dim)), 1)
+
+    # Forward pass
     for layer in self.layers:
       x = layer(x)
-    x = x + self.skip_const * nn.functional.relu(x_0)
+    
+    # Normalization
     x_a = x[:, :self.dim]
     x_b = x[:, self.dim:]
+    x_a = x_a / torch.unsqueeze(x_a.sum(dim=1), 1)
+    x_b = x_b / torch.unsqueeze(x_b.sum(dim=1), 1)
+
+    # Adding skip connection
+    x_a = x_a + self.skip_const * nn.functional.relu(x_0[:, :self.dim])
+    x_b = x_b + self.skip_const * nn.functional.relu(x_0[:, self.dim:])
+
+    # Normalization and dusting
     x_a = x_a / torch.unsqueeze(x_a.sum(dim=1), 1)
     x_b = x_b / torch.unsqueeze(x_b.sum(dim=1), 1)
     x_a = x_a + self.dust_const
@@ -47,4 +53,29 @@ class gen_net(nn.Module):
     x_a = x_a / torch.unsqueeze(x_a.sum(dim=1), 1)
     x_b = x_b / torch.unsqueeze(x_b.sum(dim=1), 1)
     x = torch.cat((x_a, x_b), dim=1)
+
+    return x
+
+# Predctive network class
+class pred_net(nn.Module):
+  """
+  Network class for the predicting the centered log of a Sinkhorn scaling factor for a pair of distributions.
+  """
+  def __init__(self, dim, width):
+    """
+    dim: dimension of each probability distribution
+    width: multiplier for width of the hidden layers
+    """
+    super(pred_net, self).__init__()
+    self.dim = dim
+    self.width = width
+    self.l1 = nn.Sequential(nn.Linear(2*dim, width*dim), nn.BatchNorm1d(width*dim), nn.ELU())
+    self.l2 = nn.Sequential(nn.Linear(width*dim, width*dim), nn.BatchNorm1d(width*dim), nn.ELU())
+    self.l3 = nn.Sequential(nn.Linear(width*dim, width*dim), nn.BatchNorm1d(width*dim), nn.ELU())
+    self.l4 = nn.Sequential(nn.Linear(width*dim, dim))
+    self.layers = [self.l1, self.l2, self.l3, self.l4]
+
+  def forward(self, x):
+    for layer in self.layers:
+      x = layer(x)
     return x
