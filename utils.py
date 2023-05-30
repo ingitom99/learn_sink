06112082aee01bc17fec12aa5f0c9ee2ae9b9e66
ingitom99 <59366443+ingitom99@users.noreply.github.com>
2023-05-30 18:47:28 +0000

    random noise plus random shapes

--- utils.py
+++ utils.py
@@ -157,9 +157,9 @@ def random_shapes_loader(n_samples, dim, dust_const):
   sample = torch.tensor(pairs)
   return sample
 
-def rn_plus_rs(n_samples, dim, dust_const):
+def rn_rs(n_samples, dim, dust_const):
   """
-  Generate a data set of pairs of samples of random shapes and randomly masked random uniform noise, randomly combined.
+  Generate a data set of pairs of samples of random shapes added to randomly-masked, uniform random noise.
 
   Inputs:
     n_samples: int
@@ -176,10 +176,10 @@ def rn_plus_rs(n_samples, dim, dust_const):
   length = int(dim**.5)
   pairs = []
   for i in range(n_samples):
-    image1 = random_shapes((length, length), max_shapes=8, min_shapes=2, min_size=4, max_size=12, channel_axis=None, allow_overlap=True)[0]
+    image1 = random_shapes((length, length), max_shapes=10, min_shapes=2, min_size=3, max_size=8, channel_axis=None, allow_overlap=True)[0]
     image1 = image1.max() - image1
     image1 = image1 / image1.sum()
-    image2= random_shapes((length, length), max_shapes=8, min_shapes=2, min_size=4, max_size=12, channel_axis=None, allow_overlap=True)[0]
+    image2= random_shapes((length, length), max_shapes=10, min_shapes=2, min_size=3, max_size=8, channel_axis=None, allow_overlap=True)[0]
     image2 = image2.max() - image2
     image2 = image2 / image2.sum()
     pair = np.concatenate((image1.flatten(), image2.flatten()))
@@ -188,18 +188,18 @@ def rn_plus_rs(n_samples, dim, dust_const):
   sample_rs = torch.tensor(pairs)
   bernoulli_p = torch.rand((n_samples, 1))
   bernoulli_p[bernoulli_p < 0.03] = 0.03
-  multiplier = torch.randint(1, 4, (n_samples, 1))
+  multiplier = torch.randint(1, 6, (n_samples, 1))
   sample_a = torch.rand((n_samples, dim))
   mask_a = torch.bernoulli(bernoulli_p * torch.ones_like(sample_a))
-  sample_a = (sample_a * mask_a)**multiplier
+  sample_a = sample_a * mask_a
   sample_a /= torch.unsqueeze(sample_a.sum(dim=1), 1)
   sample_b = torch.rand((n_samples, dim))
   mask_b = torch.bernoulli(bernoulli_p * torch.ones_like(sample_b))
-  sample_b = (sample_b * mask_b)**multiplier
+  sample_b = sample_b * mask_b
   sample_b /= torch.unsqueeze(sample_b.sum(dim=1), 1)
   sample_rn = torch.cat((sample_a, sample_b), dim=1)
-  rn_rand_fact = torch.rand((n_samples, 1))
   rs_rand_fact = torch.rand((n_samples, 1))
+  rn_rand_fact = torch.rand((n_samples, 1))
   sample = rs_rand_fact * sample_rs  + rn_rand_fact * sample_rn
   sample_a = sample[:, :784]
   sample_b = sample[:, 784:]
